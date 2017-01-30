e7557ecb5f90454f736558700eea2101d074dba8
cjredmond <connor.redmond@gmail.com>
2017-01-30 14:47:33 -0500

    fixed sql syntax error

--- flaskr.py
+++ flaskr.py
@@ -53,7 +53,7 @@ def add_entry():
     if not session.get('logged_in'):
         abort(401)
     db = get_db()
-    db.execute('instert into entries (title, text) calues(?, ?)',
+    db.execute('insert into entries (title, text) values(?, ?)',
                 [request.form['title'], request.form['text']])
     db.commit()
     flash('New entry was successfully posted')
